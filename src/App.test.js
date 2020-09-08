381e908c9398648879920c266e14faf94fbb5cfb
mmarini <marco.marini@mmarini.org>
2020-09-08 18:25:10 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Count/i);
+  const linkElement = getByText(/Count down/i);
   expect(linkElement).toBeInTheDocument();
 });
